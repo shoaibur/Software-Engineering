bed2172de73326d58e054ba5aa666209cb0d99c1
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-14 15:24:15 -0700

    Update 2020-42-311-Sparse-Matrix-Multiplication.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-42-311-Sparse-Matrix-Multiplication.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-42-311-Sparse-Matrix-Multiplication.py
@@ -1,6 +1,25 @@
 class Solution:
     def multiply(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
         '''
+        T: O(i * j * k); i=#row(A), j=#col(A), k=#col(B)
+        S: O(1), ignoring space required to store result
+        '''
+        AB = [[0] * len(B[0]) for _ in range(len(A))]
+        
+        for i in range(len(A)):
+            for j in range(len(A[0])): # same as len(B)
+                if A[i][j] == 0: continue
+                for k in range(len(B[0])):
+                    if B[j][k] == 0: continue
+                    AB[i][k] += A[i][j] * B[j][k]
+        return AB
+
+
+
+
+class Solution:
+    def multiply(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        '''
         T: O(mA * nA + mB * nB)
         S: O(mA + nB)
         '''
