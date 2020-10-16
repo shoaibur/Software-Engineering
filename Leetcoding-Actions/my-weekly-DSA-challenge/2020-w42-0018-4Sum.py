8d8295be2b28ac9096a5ce357ee23a0387bbc319
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-15 19:05:55 -0700

    Update 2020-w42-0018-4Sum.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w42-0018-4Sum.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w42-0018-4Sum.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        '''
+        T: O(n log n + n^3)
+        S: O(1)
+        '''
+        n = len(nums)
+        if n < 4: return []
+        nums.sort()
+        
+        results = set()
+        
+        for x in range(n):
+            for y in range(x+1, n):
+                i, j = y + 1, n-1
+                while i < j:
+                    summ = nums[x] + nums[y] + nums[i] + nums[j]
+                    if summ == target:
+                        results.add((nums[x],nums[y], nums[i], nums[j]))
+                        i += 1
+                    elif summ < target:
+                        i += 1
+                    else:
+                        j -= 1
+        return results
