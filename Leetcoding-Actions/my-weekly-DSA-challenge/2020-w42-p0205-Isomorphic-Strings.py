e097449c869a5c6ab13529f277e9de45021254bd
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-15 18:34:38 -0700

    Update 2020-w42-p0205-Isomorphic-Strings.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w42-p0205-Isomorphic-Strings.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w42-p0205-Isomorphic-Strings.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        '''
+        T: O(n)
+        S: O(n)
+        '''
+        if not s and not t: return True
+        if not s or not t: return False
+        
+        isoMap = {}
+        seenT = set()
+        for i in range(len(s)):
+            if s[i] not in isoMap:
+                if t[i] in seenT:
+                    return False
+                isoMap[s[i]] = t[i]
+                seenT.add(t[i])
+            elif isoMap[s[i]] != t[i]:
+                return False
+        return True
