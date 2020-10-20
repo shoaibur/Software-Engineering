fd3c1db4ca7ec134599d508f343a1af020fbe0a3
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-19 19:03:54 -0700

    Update 2020-w43-p0158-Read-N-Characters-Given-Read4-ii-Call-Multiple-Times.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0158-Read-N-Characters-Given-Read4-ii-Call-Multiple-Times.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0158-Read-N-Characters-Given-Read4-ii-Call-Multiple-Times.py
@@ -1,1 +1,23 @@
+# The read4 API is already defined for you.
+# def read4(buf4: List[str]) -> int:
 
+class Solution:
+    '''
+    T: O(n) and S: O(n)
+    '''
+    def __init__(self):
+        self.buffer = deque()
+        
+    def read(self, buf: List[str], n: int) -> int:
+        buf4 = [' '] * 4
+        while True:
+            k = read4(buf4)
+            if k == 0: break
+            for i in range(k):
+                self.buffer.append(buf4[i])
+            
+        i = 0
+        while i < n and self.buffer:
+            buf[i] = self.buffer.popleft()
+            i += 1
+        return i
