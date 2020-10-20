4fc853f1bf2c49d7e4f0f50d09c0d05c15242179
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-19 19:54:13 -0700

    Update 2020-w43-p0136-Single-Number.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0136-Single-Number.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0136-Single-Number.py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        '''
+        Solution 1: T: O(n) and O(n)
+        '''
+        # counter = {}
+        # for num in nums:
+        #     counter[num] = counter.get(num, 0) + 1
+        # for num in counter:
+        #     if counter[num] == 1:
+        #         return num
+        # return
+    
+        '''
+        Solution 2:
+        a xor b xor a = b --> unique is b
+        T: O(n) ans S: O(1)
+        '''
+        a = 0 # a xor 0 = a
+        for i in range(len(nums)):
+            a = a ^ nums[i]
+        return a
