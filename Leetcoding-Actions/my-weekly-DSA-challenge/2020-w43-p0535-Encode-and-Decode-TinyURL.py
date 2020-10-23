d6e75ba66bbafd30bc1ce5fb18cef4806995e051
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-22 23:32:58 -0500

    Update 2020-w43-p0535-Encode-and-Decode-TinyURL.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0535-Encode-and-Decode-TinyURL.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0535-Encode-and-Decode-TinyURL.py
@@ -1,1 +1,22 @@
+class Codec:
+    def __init__(self):
+        self.counter = 0
+        self.base62 = string.ascii_lowercase + string.ascii_uppercase + '0123456789'
+        self.data = {}
 
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        key = ''.join(random.Random(self.counter).sample(self.base62, 3))
+        self.data[key] = longUrl
+        return 'http://tinyurl.com/' + key
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        key = shortUrl.split('/')[-1]
+        return self.data[key]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
