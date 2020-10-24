44643a728b1f26bfc4de2f3172f5f67b824623cf
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-24 10:58:02 -0500

    Create 2020-w43-p1539-Kth-Missing-Positive-Number.py

---
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p1539-Kth-Missing-Positive-Number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        '''
+        Number of missing element at index i = 
+        = arr[0] - 1 + arr[idx] - arr[0] - idx = 
+        = arr[idx] - idx - 1
+        
+        For more description, see: 1060 Missing Element in Sorted Array
+
+        T: O(log n) and O(1)
+        
+        '''
+        
+        def countMissing(idx):
+            return arr[idx] - idx - 1
+        
+        if k < arr[0]: return k
+        
+        lo, hi = 0, len(arr)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if countMissing(mid) >= k:
+                hi = mid
+            else:
+                lo = mid + 1
+        
+        return arr[lo - 1] + k - countMissing(lo - 1)
