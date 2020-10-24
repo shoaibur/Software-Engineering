46ad18229fcb10bff25000951b1243bb80f59eb6
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-24 11:02:31 -0500

    Create 2020-w43-p0162-Find-Peak-Element.py

---
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w43-p0162-Find-Peak-Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        '''
+        T: O(log n) and S: O(1)
+        '''
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid] > nums[mid + 1]:
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
