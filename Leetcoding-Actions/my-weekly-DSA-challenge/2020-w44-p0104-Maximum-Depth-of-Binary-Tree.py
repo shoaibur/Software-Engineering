e6a8022563fa92ef6601e72d268f4507ae04b2da
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-26 19:02:04 -0500

    Create 2020-w44-p0104-Maximum-Depth-of-Binary-Tree.py

---
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w44-p0104-Maximum-Depth-of-Binary-Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        if not root.left and not root.right:
+            return 1
+        if not root.left:
+            return self.maxDepth(root.right) + 1
+        if not root.right:
+            return self.maxDepth(root.left) + 1
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
