7d410a272dcdb8df9c564eda11b8be30d8ead0db
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-27 19:07:30 -0500

    Create 2020-w44-p0142-Linked-List-Cycle-ii.py

---
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w44-p0142-Linked-List-Cycle-ii.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        '''
+        T: O(n) and S: O(1)
+        '''        
+        def findIntersection(head):
+            slow = head
+            fast = head
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+                if slow == fast:
+                    return slow
+            return None
+        
+        if not head: return None
+        intersect = findIntersection(head)
+        if not intersect: return None
+        
+        i, j = head, intersect
+        while i != j:
+            i = i.next
+            j = j.next
+        
+        return i
