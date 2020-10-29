87d443d437b49533fa655345b843f1e012266b97
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-28 19:25:36 -0700

    Update 2020-w44-p0896-Monotonic-Array.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w44-p0896-Monotonic-Array.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w44-p0896-Monotonic-Array.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        '''
+        T: O(n) and S: O(n)
+        '''
+        
+        def isIncreasing(nums):
+            for i in range(1, len(nums)):
+                if nums[i-1] > nums[i]:
+                    return False
+            return True
+        
+        return isIncreasing(A) or isIncreasing(A[::-1])
