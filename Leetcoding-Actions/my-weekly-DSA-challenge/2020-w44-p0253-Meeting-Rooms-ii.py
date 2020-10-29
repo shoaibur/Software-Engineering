cd39467a6c02f05a25a0d9b31f4f9d1535b1b0b2
Shoaibur Rahman <shoaibeee@gmail.com>
2020-10-28 18:33:34 -0700

    Update 2020-w44-p0253-Meeting-Rooms-ii.py

--- Leetcoding-Actions/my-weekly-DSA-challenge/2020-w44-p0253-Meeting-Rooms-ii.py
+++ Leetcoding-Actions/my-weekly-DSA-challenge/2020-w44-p0253-Meeting-Rooms-ii.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        '''
+        T: O(n log n) and S: O(n)
+        '''
+        if not intervals: return 0
+        
+        start = sorted([interval[0] for interval in intervals])
+        end = sorted([interval[1] for interval in intervals])
+        
+        rooms = 0
+        i, j = 0, 0
+        while i < len(start):
+            if start[i] < end[j]:
+                rooms += 1
+            else:
+                j += 1
+            i += 1
+        
+        return rooms
