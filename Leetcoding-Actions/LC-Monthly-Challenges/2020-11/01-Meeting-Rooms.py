4e16afdca207cc9e3186c0eef3acff61eb53e631
Shoaibur Rahman <shoaibeee@gmail.com>
2020-11-01 07:08:13 -0600

    Update 01-Meeting-Rooms.py

--- Leetcoding-Actions/LC-Monthly-Challenges/2020-11/01-Meeting-Rooms.py
+++ Leetcoding-Actions/LC-Monthly-Challenges/2020-11/01-Meeting-Rooms.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        '''
+        T: O(n log n) and S: O(1)
+        '''
+        if not intervals: return True
+        
+        intervals.sort()
+        
+        i1 = intervals[0]
+        for i2 in intervals[1:]:
+            if i1[1] > i2[0]:
+                return False
+            else:
+                i1 = i2
+                
+        return True
