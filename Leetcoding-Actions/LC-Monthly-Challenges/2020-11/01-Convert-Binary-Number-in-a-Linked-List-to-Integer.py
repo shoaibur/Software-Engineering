4f15129467f00b9c3a685892616687795cc10f96
Shoaibur Rahman <shoaibeee@gmail.com>
2020-11-01 07:00:43 -0600

    Update 01-Convert-Binary-Number-in-a-Linked-List-to-Integer.py

--- Leetcoding-Actions/LC-Monthly-Challenges/2020-11/01-Convert-Binary-Number-in-a-Linked-List-to-Integer.py
+++ Leetcoding-Actions/LC-Monthly-Challenges/2020-11/01-Convert-Binary-Number-in-a-Linked-List-to-Integer.py
@@ -1,1 +1,30 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        '''
+        T: O(n) and S: O(1)
+        '''
+        def reverseLinkedList(head):
+            prev = None
+            tail = head
+            while tail:
+                temp = tail.next
+                tail.next = prev
+                prev = tail
+                tail = temp
+            return prev
+        
+        head = reverseLinkedList(head)
+        i, decimal = 0, 0
+        tail = head
+        while tail:
+            if tail.val:
+                decimal += tail.val * 2 ** i
+            tail = tail.next
+            i += 1
+            
+        return decimal
