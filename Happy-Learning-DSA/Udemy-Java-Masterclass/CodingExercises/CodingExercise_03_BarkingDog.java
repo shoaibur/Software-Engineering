9bffaf4cca871f7333815be8a1ddd255597dc69b
Shoaibur Rahman <shoaibeee@gmail.com>
2020-11-01 18:39:50 -0800

    Create CodingExercise_03_BarkingDog.java

---
+++ Happy-Learning-DSA/Udemy-Java-Masterclass/CodingExercises/CodingExercise_03_BarkingDog.java
@@ -0,0 +1,12 @@
+public class BarkingDog {
+ 
+ public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+    if (!barking || hourOfDay < 0 || hourOfDay > 23 ) {
+        return false;
+    }
+    if (hourOfDay < 8 || hourOfDay > 22) {
+        return true;
+    }
+    return false;
+ }
+}
