f5b2166d3b13de88ffda37678ad92c1c87952740
Shoaibur Rahman <shoaibeee@gmail.com>
2020-11-02 17:52:27 -0800

    Update 02-Insertion-Sort_List.py

--- Leetcoding-Actions/LC-Monthly-Challenges/2020-11/02-Insertion-Sort_List.py
+++ Leetcoding-Actions/LC-Monthly-Challenges/2020-11/02-Insertion-Sort_List.py
@@ -1,1 +1,34 @@
-
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        '''
+        T: O(n^2) and S: O(n)
+        '''
+        nums = []
+        def getNums(head):
+            tail = head
+            while tail:
+                nums.append(tail.val)
+                tail = tail.next
+        
+        def insertionSort(nums):
+            i = 1
+            while i < len(nums):
+                j = i - 1
+                while j >= 0:
+                    if nums[j] > nums[j+1]:
+                        nums[j], nums[j+1] = nums[j+1], nums[j]
+                    j -= 1
+                i += 1
+        
+        if not head: return None
+        getNums(head)
+        insertionSort(nums)
+        for i, num in enumerate(nums):
+            if i == 0:
+                head = ListNode(num)
+                tail = head
+            else:
+                tail.next = ListNode(num)
+                tail = tail.next
+        
+        return head
