9cc246abf0842a7583325cb4903f739ff7c4e197
Shoaibur Rahman <shoaibeee@gmail.com>
2020-11-04 13:40:55 -0800

    Create CodingExercise_17_EvenDigitSum.java

---
+++ Happy-Learning-DSA/Udemy-Java-Masterclass/CodingExercises/CodingExercise_17_EvenDigitSum.java
@@ -0,0 +1,14 @@
+public class EvenDigitSum {
+    
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) return -1;
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 2 == 0)
+                sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
