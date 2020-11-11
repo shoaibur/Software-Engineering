aa64b2f8147526230915a2b1558ac8674864d154
Shoaibur Rahman <shoaibeee@gmail.com>
2020-11-10 20:04:36 -0800

    Update CodingExercise_27_InputCalculator.java

--- Happy-Learning-DSA/Udemy-Java-Masterclass/CodingExercises/CodingExercise_27_InputCalculator.java
+++ Happy-Learning-DSA/Udemy-Java-Masterclass/CodingExercises/CodingExercise_27_InputCalculator.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class InputCalculator {
+    
+    public static void inputThenPrintSumAndAverage() {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        double count = 0;
+        while (sc.hasNextInt()) {
+            sum += sc.nextInt();
+            count += 1;
+        }
+        long avg = (long) (Math.round(sum / count));
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+    }
+}
