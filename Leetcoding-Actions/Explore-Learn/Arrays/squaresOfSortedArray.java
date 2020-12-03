6070621df3317472c9bcf38fc21b03b006a35794
Shoaibur Rahman <shoaibeee@gmail.com>
2020-12-02 21:44:01 -0600

    Create squaresOfSortedArray.java

---
+++ Leetcoding-Actions/Explore-Learn/Arrays/squaresOfSortedArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int i = 0;
+        int j = nums.length - 1;
+        int k = nums.length - 1;
+        
+        while (i <= j) {
+            if (Math.abs(nums[j]) > Math.abs(nums[i])) {
+                result[k] = nums[j] * nums[j];
+                j--;
+            } else {
+                result[k] = nums[i] * nums[i];
+                i++;
+            }
+            k--;
+        }
+        return result;
+    }
+}
