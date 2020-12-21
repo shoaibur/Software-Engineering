9209411eab4ecbd35c4684873595f9b9bbb6206b
Shoaibur Rahman <shoaibeee@gmail.com>
2020-12-21 08:34:26 -0600

    Update reverseInteger.py

--- Interview-Preparation/Google/8-Others/reverseInteger.py
+++ Interview-Preparation/Google/8-Others/reverseInteger.py
@@ -1,3 +1,24 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        return int(str(x)[::-1])
+        
+        negative = x < 0
+        x = abs(x)
+        y = x
+        
+        nDigit = 0
+        while x > 0:
+            x //= 10
+            nDigit += 1
+        
+        result = 0
+        while y > 0:
+            y, rem = divmod(y, 10)
+            result += rem * 10 ** (nDigit - 1)
+            nDigit -= 1
+        
+        if negative:
+            result = -result
+        overflow = (result < -2**31) or (result > 2**31 - 1)
+        if overflow: return 0
+        
+        return result
