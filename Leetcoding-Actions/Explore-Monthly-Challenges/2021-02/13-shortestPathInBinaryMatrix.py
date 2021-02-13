aa882bb4fba9d436f828ec416d20af27521fb305
Shoaibur Rahman <shoaibeee@gmail.com>
2021-02-13 16:12:59 +0000

    Update 13-shortestPathInBinaryMatrix.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-02/13-shortestPathInBinaryMatrix.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-02/13-shortestPathInBinaryMatrix.py
@@ -1,3 +1,45 @@
+# Using BFS
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        
+        # Helper function to find the neighbors of a given cell.
+        def get_neighbours(row, col):
+            for row_difference, col_difference in directions:
+                new_row = row + row_difference
+                new_col = col + col_difference
+                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
+                    continue
+                if grid[new_row][new_col] != 0:
+                    continue
+                yield (new_row, new_col)
+        
+        # Check that the first and last cells are open. 
+        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
+            return -1
+        
+        # Set up the BFS.
+        queue = deque()
+        queue.append((0, 0))
+        grid[0][0] = 1 
+        
+        # Carry out the BFS.
+        while queue:
+            row, col = queue.popleft()
+            distance = grid[row][col]
+            if (row, col) == (max_row, max_col):
+                return distance
+            for neighbour_row, neighbour_col in get_neighbours(row, col):
+                grid[neighbour_row][neighbour_col] = distance + 1
+                queue.append((neighbour_row, neighbour_col))
+        
+        # There was no path.
+        return -1
+    
 # Using A*
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
