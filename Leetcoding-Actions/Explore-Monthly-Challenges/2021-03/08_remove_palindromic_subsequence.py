8fc82b03caa33644f28e9265cb50f81f6a60c828
Shoaibur Rahman <shoaibeee@gmail.com>
2021-03-08 14:06:27 +0000

    Update 08_remove_palindromic_subsequence.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/08_remove_palindromic_subsequence.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/08_remove_palindromic_subsequence.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        '''
+        T: O(n) and S: O(1)
+        '''
+        def is_palindrome(s):
+            i, j = 0, len(s) - 1
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i, j = i + 1, j - 1
+            return True
+        
+        if not s:
+            return 0
+        if is_palindrome(s):
+            return 1
+        return 2
