aa251092869c9057541892ce3ce47cf76f39f424
Shoaibur Rahman <shoaibeee@gmail.com>
2021-03-12 14:42:42 +0000

    Update 12_check_if_a_string_contains_all_binary_codes_of_size_k.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/12_check_if_a_string_contains_all_binary_codes_of_size_k.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/12_check_if_a_string_contains_all_binary_codes_of_size_k.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        '''
+        T: O(nk) and S: O(nk)
+        '''
+        checked = {s[i-k:i] for i in range(k, len(s)+1)}
+        
+        return len(checked) == (1 << k)
