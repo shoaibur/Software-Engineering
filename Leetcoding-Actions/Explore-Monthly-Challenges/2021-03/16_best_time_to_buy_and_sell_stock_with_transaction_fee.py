91f9a38860b3e9fbf4721cce60096e94a8cb9664
Shoaibur Rahman <shoaibeee@gmail.com>
2021-03-16 13:09:27 +0000

    Update 16_best_time_to_buy_and_sell_stock_with_transaction_fee.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/16_best_time_to_buy_and_sell_stock_with_transaction_fee.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/16_best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        '''
+        T: O(n) and S: O(1)
+        '''
+        cash, hold = 0, -prices[0]
+        for i in range(1, len(prices)):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+        return cash
