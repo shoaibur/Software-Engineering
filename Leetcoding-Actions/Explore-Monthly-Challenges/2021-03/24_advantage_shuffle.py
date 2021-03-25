b63e8b6c185cf5fbec6dbcb4ad47ad593d75030f
Shoaibur Rahman <shoaibeee@gmail.com>
2021-03-25 11:51:11 -0500

    Update 24_advantage_shuffle.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/24_advantage_shuffle.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-03/24_advantage_shuffle.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
+        A.sort()
+        result = []
+        counter = defaultdict(list)
+        for idx in sorted(B, reverse=True):
+            if A[-1] > idx: 
+                counter[idx].append(A.pop())
+        for idx in B:
+            if counter[idx]:
+                result.append(counter[idx].pop())
+            else:
+                result.append(A.pop())
+        return result
