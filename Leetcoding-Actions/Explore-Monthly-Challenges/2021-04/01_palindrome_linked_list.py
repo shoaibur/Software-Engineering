1519e7ebd476005119df672fbefe047492709ca1
Shoaibur Rahman <shoaibeee@gmail.com>
2021-04-01 08:46:47 +0000

    Update 01_palindrome_linked_list.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-04/01_palindrome_linked_list.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-04/01_palindrome_linked_list.py
@@ -1,1 +1,42 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head: return True
+        if head.next is None: return True
+        if head.next.next is None:
+            return head.val == head.next.val
+        # split into two halves
+        slow = head
+        fast = head.next
+        while fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast.next is None:
+                break
+        right = slow.next
+        slow.next = None
+        left = head
+        
+        # Reverse second half
+        prev = None
+        tail = right
+        while tail:
+            temp = tail.next
+            tail.next = prev
+            prev = tail
+            tail = temp
+        right = prev
+        
+        # compare first and second half
+        left_tail = left
+        right_tail = right
+        while left_tail:
+            if left_tail.val != right_tail.val:
+                return False
+            left_tail = left_tail.next
+            right_tail = right_tail.next
+        return True
