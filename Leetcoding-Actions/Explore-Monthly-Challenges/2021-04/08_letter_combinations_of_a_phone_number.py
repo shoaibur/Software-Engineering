d971290db9a8ea2e86b5a8e7bcc960d850213b8c
Shoaibur Rahman <shoaibeee@gmail.com>
2021-04-09 09:43:04 +0000

    Update 08_letter_combinations_of_a_phone_number.py

--- Leetcoding-Actions/Explore-Monthly-Challenges/2021-04/08_letter_combinations_of_a_phone_number.py
+++ Leetcoding-Actions/Explore-Monthly-Challenges/2021-04/08_letter_combinations_of_a_phone_number.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        maps = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        
+        words = [maps[digit] for digit in digits]
+        
+        result = [char for char in words[0]]
+        
+        for word in words[1:]:
+            temp = []
+            for char in word:
+                for item in result:
+                    temp.append(item + char)
+            result = temp
+        return result
